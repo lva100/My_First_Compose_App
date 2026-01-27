ba32e18944896b3354f6bd2a20512437edeedbd5
lva100 <lva100@yandex.ru>
2026-01-27 15:16:52 +0300

    Lesson 9_3

---
+++ app/src/main/java/dev/lva100/myapp/ui/theme/VKNewsClientTheme.kt
@@ -0,0 +1,44 @@
+package dev.lva100.myapp.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Black900,
+    secondary = Black900,
+    onPrimary = Color.White,
+    onSecondary = Black500,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color.White,
+    secondary = Color.White,
+    onPrimary = Black900,
+    onSecondary = Black500,
+)
+
+@Composable
+fun VKNewsClientTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
--- app/src/main/java/dev/lva100/myapp/ui/theme/VKNewsMainScreen.kt
+++ app/src/main/java/dev/lva100/myapp/ui/theme/VKNewsMainScreen.kt
@@ -1,46 +1,103 @@
 package dev.lva100.myapp.ui.theme
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
-    Scaffold(
-        bottomBar = {
-            BottomAppBar{
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceAround
-                ) {
-                    val items = listOf(NavigationItem.Home, NavigationItem.Favourite,
-                        NavigationItem.Profile)
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = "Избранное")
-                    }
-                    IconButton(
-                        onClick = { },
-//                    modifier = Modifier.weight(1f, true)
-                    ) {
-                        Icon(Icons.Filled.Edit, contentDescription = "Редактировать")
-                    }
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Filled.Delete, contentDescription = "Удалить")
-                    }
-                }
-            }
-        }
-    ) {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember { mutableStateOf(true) }
 
+    Scaffold(
+//        containerColor = Color.White,
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }, floatingActionButton = {
+            if (fabIsVisible.value == true) {
+            FloatingActionButton(
+                onClick = {
+                    scope.launch {
+                        val action = snackbarHostState.showSnackbar(
+                            message = "This is snackbar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                }) {
+                Icon(Icons.Filled.Favorite, contentDescription = null)
+            }}
+        }, bottomBar = {
+            NavigationBar(
+//                containerColor = Color(204,204,204,30),
+            ) {
+                Log.d("COMPOSE_TEST", "BottomNavigation")
+
+                val selectedItemPosition = remember { mutableStateOf(0) }
+
+                val items = listOf(
+                    NavigationItem.Home, NavigationItem.Favourite, NavigationItem.Profile
+                )
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        onClick = { selectedItemPosition.value = index },
+                        selected = selectedItemPosition.value == index,
+                        icon = {
+                            Icon(item.icon, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = stringResource(id = item.titleResId))
+                        },
+                        colors = NavigationBarItemColors(
+                            selectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                            selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                            selectedIndicatorColor = Color(234, 221, 255, 255),
+                            unselectedIconColor = MaterialTheme.colorScheme.onSecondary,
+                            unselectedTextColor = MaterialTheme.colorScheme.onSecondary,
+                            disabledIconColor = Color.LightGray,
+                            disabledTextColor = Color.LightGray,
+                        )
+                    )
+                }
+
+            }
+        }) {
+        Text(text = "123")
+    }
+}
+
+@Preview
+@Composable
+fun testUI() {
+    VKNewsClientTheme(
+        darkTheme = false
+    ) {
+        MainScreen()
     }
 }
