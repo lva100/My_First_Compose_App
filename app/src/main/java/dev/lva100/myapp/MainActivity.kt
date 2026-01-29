48bc3f9a83d8bfc853d6bb44ff6006e5d10ded03
lva100 <lva100@yandex.ru>
2026-01-29 16:52:21 +0300

    Lesson11_2

--- app/src/main/java/dev/lva100/myapp/MainActivity.kt
+++ app/src/main/java/dev/lva100/myapp/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.ViewModelProvider
@@ -42,9 +44,15 @@ private fun Test(viewModel: MainViewModel) {
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
         ) {
+            val models = viewModel.models.observeAsState(listOf())
             LazyColumn {
-                items(500) {
-                    InstagramProfileCard(viewModel)
+                items(models.value) { model ->
+                    InstagramProfileCard(
+                        model,
+                        onFollowedButtonClickListener = {
+                            viewModel.changeFollowingStatus(it)
+                        }
+                    )
                 }
             }
         }
--- app/src/main/java/dev/lva100/myapp/MainViewModel.kt
+++ app/src/main/java/dev/lva100/myapp/MainViewModel.kt
@@ -3,15 +3,38 @@ package dev.lva100.myapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.lva100.myapp.domain.InstagramModel
+import kotlin.random.Random
 
 class MainViewModel: ViewModel() {
 
-    private val _isFollowing = MutableLiveData<Boolean>()
-    val isFollowing: LiveData<Boolean> = _isFollowing
-
-    fun changeFollowingStatus() {
-        val wasFollowing = _isFollowing.value ?: false
-        _isFollowing.value = !wasFollowing
+    private val initialList = mutableListOf<InstagramModel>().apply {
+        repeat(500) {
+            add(
+                InstagramModel(
+                    id = it,
+                    title = "Title: $it",
+                    isFollowed = Random.nextBoolean()
+                )
+            )
+        }
     }
+
+    private val _models = MutableLiveData<List<InstagramModel>>(initialList)
+
+    val models: LiveData<List<InstagramModel>> = _models
+
+    fun changeFollowingStatus(model: InstagramModel) {
+        val modifiedList = _models.value?.toMutableList() ?: mutableListOf()
+        modifiedList.replaceAll {
+            if (it == model) {
+                it.copy(isFollowed = !it.isFollowed)
+            } else {
+                it
+            }
+        }
+        _models.value = modifiedList
+    }
+
 
 }
