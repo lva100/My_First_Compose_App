d18fff3e2f1a119136b264e7ff6f9830fa2627af
lva100 <lva100@yandex.ru>
2026-02-02 12:37:56 +0300

    Navigation without libraries

---
+++ app/src/main/java/dev/lva100/myapp/ui/theme/HomeScreen.kt
@@ -0,0 +1,88 @@
+package dev.lva100.myapp.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.lva100.myapp.VKNewsViewModel
+
+@Composable
+fun HomeScreen(
+    viewModel: VKNewsViewModel,
+    paddingValues: PaddingValues
+) {
+    val feedPosts = viewModel.feedPosts.observeAsState(listOf())
+
+    LazyColumn(
+        modifier = Modifier.padding(paddingValues),
+        contentPadding = PaddingValues(
+            top = 16.dp,
+            start = 8.dp,
+            end = 8.dp,
+            bottom = 74.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    )
+    {
+        items(
+            items = feedPosts.value,
+            key = { it.id }
+        ) { feedPost ->
+            val dismissState = rememberSwipeToDismissBoxState()
+            SwipeToDismissBox(
+                modifier = Modifier.animateItem(),
+                state = dismissState,
+                enableDismissFromStartToEnd = false,
+                backgroundContent = {
+                    Box(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxSize()
+                            .background(Color.Red.copy(alpha = 0.5f)),
+                        contentAlignment = Alignment.CenterEnd
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(16.dp),
+                            text = "Delete Item",
+                            color = Color.White,
+                            fontSize = 24.sp
+                        )
+                    }
+                },
+                onDismiss = {
+                    viewModel.remove(feedPost)
+                }
+            ) {
+                PostCard(
+                    feedPost = feedPost,
+                    onViewsItemClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
+                    onShareItemClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
+                    onLikeItemClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
+                    onCommentItemClickListener = { statisticItem ->
+                        viewModel.updateCount(feedPost, statisticItem)
+                    },
+                )
+            }
+        }
+    }
+}
